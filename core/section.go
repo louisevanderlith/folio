9ba7f1612f149f668e57aa9daf50b262677c7838
louisevanderlith <louisevanderlith@gmail.com>
2020-10-30 11:28:06 +0200

    Changed TimeKey to Ptr

--- core/section.go
+++ core/section.go
@@ -8,5 +8,5 @@ type Section struct {
 	Heading  string  `hsk:"size(50)"`
 	Text     string  `hsk:"size(512)"`
 	ImageUrl string  `hsk:"null"`
-	ImageKey keys.TimeKey `hsk:"null"`
+	ImageKey *keys.TimeKey `hsk:"null"`
 }
